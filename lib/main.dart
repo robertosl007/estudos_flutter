afca6b2ab2a85ff8c75d12a1cb0b334695a6fd45
robertosl007 <roberto@rgfsistemas.com.br>
2023-04-06 17:02:06 -0300

    Criando botões contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -48,68 +50,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  final foto = const CircleAvatar(
+    backgroundImage: NetworkImage(
+        'https://pps.whatsapp.net/v/t61.24694-24/104249803_2991737727614470_8174073353209416045_n.jpg?ccb=11-4&oh=01_AdRg5ZU9fpTd8bWzUz8JyAWhp1FMNPt8lF_-WnICUvwEzQ&oe=643BBD37'),
+    radius: 150,
+  );
+  final nome = const Text(
+    'Roberto da Silva Luz',
+    style: TextStyle(fontSize: 30),
+    textAlign: TextAlign.center,
+  );
+  final botaoTelefone = IconButton(
+    color: Colors.blue,
+    icon: const Icon(Icons.phone),
+    onPressed: () {
+      launchUrl(Uri(scheme: 'tel', path: '63992579391'));
+    },
+  );
+  final botaoEmail = IconButton(
+    color: Colors.blue,
+    icon: const Icon(Icons.mail),
+    onPressed: () {
+      launchUrl(Uri(scheme: 'mailto', path: 'roberto.silva007@gmail.com'));
+    },
+  );
+  final botaoSMS = IconButton(
+    color: Colors.blue,
+    icon: const Icon(Icons.sms),
+    onPressed: () {
+      launchUrl(Uri(scheme: 'sms', path: '63992579391'));
+    },
+  );
+  Widget build(BuildContext contexto) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        foto,
+        nome,
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Clique no botão aí embaixo, maluco:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: [botaoTelefone, botaoEmail, botaoSMS],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ],
+    ));
   }
 }
